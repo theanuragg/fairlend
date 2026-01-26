d5a855e9c48813f45f93c8c58e5f4d8e8bfc4d68
anurag <anurags9693@gmail.com>
2026-01-26 11:28:41 +0530

    Add ReserveLiquidity struct
    - Introduced the `ReserveLiquidity` struct to manage reserve liquidity details, including mint address, supply address, and liquidity amounts.
    - Implemented methods for liquidity management, including deposit, withdraw, borrow, and repay functionalities.
    - Added error handling for liquidity operations to ensure robust performance within the lending framework.

---
+++ program/fairlend/programs/fairlend/src/account_data/reserve/reserve_liquidity.rs
@@ -0,0 +1,140 @@
+use crate::{
+    errors::LendingError,
+    math::common::{Decimal, WAD},
+    utils::byte_length::ByteLength,
+};
+use anchor_lang::prelude::*;
+
+#[derive(Clone, Copy, Debug, Default, AnchorSerialize, AnchorDeserialize)]
+pub struct ReserveLiquidity {
+    /// Reserve liquidity mint address
+    pub mint_pubkey: Pubkey,
+
+    /// Reserve liquidity mint decimals
+    pub mint_decimals: u8,
+
+    /// Reserve liquidity supply address
+    pub supply_pubkey: Pubkey,
+
+    /// Reserve liquidity fee receiver address
+    pub fee_receiver: Pubkey,
+
+    /// Reserve liquidity oracle account
+    pub oracle_pubkey: Pubkey,
+
+    /// Reserve liquidity available
+    pub available_amount: u64,
+
+    /// Reserve liquidity borrowed
+    pub borrowed_amount_wads: u128,
+
+    /// Reserve liquidity cumulative borrow rate
+    pub cumulative_borrow_rate_wads: u128,
+
+    /// Reserve liquidity market price in quote currency
+    pub market_price: u128,
+}
+
+impl ByteLength for ReserveLiquidity {
+    const LEN: usize = 32 + 1 + 32 + 32 + 32 + 8 + 16 + 16 + 16;
+}
+
+pub struct NewReserveLiquidityParams {
+    /// Reserve liquidity mind address
+    pub mint_pubkey: Pubkey,
+
+    /// Reserve liquidity mint decimals
+    pub mint_decimals: u8,
+
+    /// Reserve liquidity supply address
+    pub supply_pubkey: Pubkey,
+
+    /// Reserve liquidity fee receiver address
+    pub fee_receiver: Pubkey,
+
+    /// Reserve liquidity oracle account
+    pub oracle_pubkey: Pubkey,
+
+    /// Reserve liquidity market price in quote currency
+    pub market_price: u128,
+}
+
+impl ReserveLiquidity {
+    pub fn new(params: NewReserveLiquidityParams) -> Self {
+        Self {
+            mint_pubkey: params.mint_pubkey,
+            mint_decimals: params.mint_decimals,
+            supply_pubkey: params.supply_pubkey,
+            fee_receiver: params.fee_receiver,
+            oracle_pubkey: params.oracle_pubkey,
+            available_amount: 0,
+            borrowed_amount_wads: 0,
+            cumulative_borrow_rate_wads: WAD as u128,
+            market_price: params.market_price,
+        }
+    }
+
+    /// Returns the total supply capability including borrowed token amount
+    pub fn total_supply(&self) -> Result<u128> {
+        (self.available_amount as u128)
+            .checked_add(self.borrowed_amount_wads)
+            .ok_or(error!(LendingError::MathOverflow))
+    }
+
+    /// Deposit liquidity
+    pub fn deposit(&mut self, liquidity_amount: u64) -> Result<()> {
+        self.available_amount = self
+            .available_amount
+            .checked_add(liquidity_amount)
+            .ok_or(LendingError::MathOverflow)?;
+
+        Ok(())
+    }
+
+    /// Withdraw liquidity
+    pub fn withdraw(&mut self, liquidity_amount: u64) -> Result<()> {
+        require_gte!(
+            self.available_amount,
+            liquidity_amount,
+            LendingError::InsufficientLiquidity
+        );
+
+        self.available_amount = self
+            .available_amount
+            .checked_sub(liquidity_amount)
+            .ok_or(LendingError::MathOverflow)?;
+
+        Ok(())
+    }
+
+    pub fn borrow_(&mut self, borrow_decimal: u128) -> Result<()> {
+        let borrow_amount = borrow_decimal.try_floor_u64()?;
+        require_gte!(
+            self.available_amount,
+            borrow_amount,
+            LendingError::InsufficientLiquidity
+        );
+
+        self.available_amount
+            .checked_sub(borrow_amount)
+            .ok_or(LendingError::MathOverflow)?;
+        self.borrowed_amount_wads = self
+            .borrowed_amount_wads
+            .checked_add(borrow_decimal)
+            .ok_or(LendingError::MathOverflow)?;
+
+        Ok(())
+    }
+
+    pub fn repay(&mut self, repay_amount: u64, settle_amount: u128) -> Result<()> {
+        self.available_amount = self
+            .available_amount
+            .checked_add(repay_amount)
+            .ok_or(LendingError::MathOverflow)?;
+        self.borrowed_amount_wads = self
+            .borrowed_amount_wads
+            .checked_sub(settle_amount)
+            .ok_or(LendingError::MathOverflow)?;
+        Ok(())
+    }
+}
