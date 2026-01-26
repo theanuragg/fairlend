e21e8ca2085712e4192d53a38d33c0a982ce511f
anurag <anurags9693@gmail.com>
2026-01-26 11:31:06 +0530

    Add constants and error handling for lending program
    - Introduced a new `constants.rs` file to define key constants such as `PROGRAM_VERSION`, `INITIAL_COLLATERAL_RATIO`, and `MAX_OBLIGATION_RESERVE`.
    - Added a new `errors.rs` file to encapsulate various error codes related to lending operations, enhancing error handling and clarity in the lending framework.

---
+++ program/fairlend/programs/fairlend/src/errors.rs
@@ -0,0 +1,91 @@
+use anchor_lang::prelude::*;
+
+#[error_code]
+pub enum LendingError {
+    #[msg("Instruction Unpack Error")]
+    InstructionUnpackError,
+
+    #[msg("Market owner is invalid")]
+    InvalidMarketOwner,
+
+    #[msg("Input amount is invalid")]
+    InvalidAmount,
+
+    #[msg("Input config value is invalid")]
+    InvalidConfig,
+
+    #[msg("Reserve must be initialized with liquidity")]
+    ReserveNotInitializedWithLiquidity,
+
+    #[msg("Invalid account input")]
+    InvalidAccountInput,
+
+    #[msg("Input oracle config is invalid")]
+    InvalidOracleConfig,
+
+    #[msg("Math operation overflow")]
+    MathOverflow,
+
+    #[msg("Pubkey error")]
+    PubkeyError,
+
+    #[msg("Market authority is invalid")]
+    InvalidMarketAuthority,
+
+    #[msg("Insufficient liquidity available")]
+    InsufficientLiquidity,
+
+    #[msg("Stale Reserve")]
+    ReserveStale,
+
+    #[msg("Invalid Token Program ID")]
+    InvalidTokenProgram,
+
+    #[msg("Obligation Reserve accounts exceeds the limit")]
+    ObligationReserveLimit,
+
+    #[msg("Stale Obligation")]
+    ObligationStale,
+
+    #[msg("Obligation deposits is empty")]
+    ObligatinoDepositsEmpty,
+
+    #[msg("Obligation deposit amount is zero")]
+    ObligationDepositsZero,
+
+    #[msg("Deposited Obligation Collateral is empty")]
+    ObligationCollateralEmpty,
+
+    #[msg("Obligation collatateral is invalid")]
+    InvalidObligationCollateral,
+
+    #[msg("Withdraw amount is too large")]
+    WithdrawTooLarge,
+
+    #[msg("Withdraw amount is too small")]
+    WithdrawTooSmall,
+
+    #[msg("Account owner is invalid")]
+    InvalidAccountOwner,
+
+    #[msg("Obligation owner is invalid")]
+    InvalidObligationOwner,
+
+    #[msg("Borrow value is too large")]
+    BorrowTooLarge,
+
+    #[msg("Borrow value is too small")]
+    BorrowTooSmall,
+
+    #[msg("Obligation borrows is empty")]
+    ObligationBorrowsEmpty,
+
+    #[msg("Obligation liquidity is invalid")]
+    InvalidObligationLiquidity,
+
+    #[msg("Obligation liquidity is empty")]
+    ObligationLiquidityEmpty,
+
+    #[msg("Repay amount is too small")]
+    RepayTooSmall,
+}
